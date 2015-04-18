bb6ef019a89d5d70308520fbd60ce0c921b115ed
Rafael1981 <r.a.ferreira1981@gmail.com>
2015-04-19 00:20:45 +1000

    Associate listing with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,4 +13,7 @@ validates_attachment_content_type :image,
 validates :name, :description, :price, presence: true
 validates :price, numericality: { greater_than: 0 }
 validates_attachment_presence :image
+
+
+belongs_to :user
 end
